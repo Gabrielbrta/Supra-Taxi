7a3f87aae5e731c77dd959029900c40315522da9
Gabrielbrta <vdvsa159946@gmail.com>
2026-06-27 11:46:18 -0300

    feat: CVA adicionado ao input-select

--- src/app/shared/components/forms/input-select/input-select.component.ts
+++ src/app/shared/components/forms/input-select/input-select.component.ts
@@ -1,5 +1,5 @@
-import { Component, input } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Component, forwardRef, input } from '@angular/core';
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -8,13 +8,54 @@ import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-input-select',
-  imports: [MatFormFieldModule, MatSelectModule, MatInputModule, FormsModule, ReactiveFormsModule, MatTooltip],
+  imports: [
+    MatFormFieldModule, 
+    MatSelectModule, 
+    MatInputModule, 
+    FormsModule, 
+    ReactiveFormsModule, 
+    MatTooltip
+  ],
   templateUrl: './input-select.component.html',
   styleUrl: './input-select.component.scss',
+
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputSelectComponent),
+      multi: true
+    }
+  ]
 })
-export class InputSelectComponent {
+export class InputSelectComponent implements ControlValueAccessor {
   selectSource = input.required<PageResult<any>>()
   label = input.required<string>();
   required = input<boolean>();
+  value: string | null = null;
+  disabled = false;
 
+  private onChange = (value: string | null ) => {}
+  private onTouched = () => {}
+
+  writeValue(value: string | null): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = this.disabled;
+  }
+
+  onSelectionChange(value: string) {
+    this.value = value;
+    this.onChange(value);
+    this.onTouched();
+  }
 }
