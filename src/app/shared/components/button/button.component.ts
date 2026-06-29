c11ff25bdb9f6540cae26c030c8b8565ec3071e3
Gabrielbrta <vdvsa159946@gmail.com>
2026-06-28 22:57:21 -0300

    style: lógica ajustada e estilização

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -3,14 +3,15 @@ import { MatButtonAppearance, MatButtonModule } from '@angular/material/button';
 import { Icons } from '../../icons/icons';
 import { RouterLink } from '@angular/router';
 import { LucideDynamicIcon } from '@lucide/angular';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-button',
-  imports: [ MatButtonModule, RouterLink, LucideDynamicIcon],
+  imports: [ MatButtonModule, RouterLink, LucideDynamicIcon, MatTooltipModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
 })
-export class ButtonComponent {
+export class ButtonComponent{
   labelButton = input<string | null>();
   icons = Icons;
   icon = input<keyof typeof Icons | null>();
@@ -18,8 +19,9 @@ export class ButtonComponent {
   routerLink = input<string | null>(null);
   exportable = input<boolean | null>(null);
   disabled = input<boolean>();
+  tooltip = input<string>();
   clicked = output<MouseEvent>();
-  
+
   onClick(event: MouseEvent) {
     this.clicked.emit(event);
   }
